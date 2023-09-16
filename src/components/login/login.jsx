a5ef33ccc41f469dbc71d0a66584a0796a7a9d21
Ezequiel Gorosito <mauro.gorosito@25watts.ar>
2023-09-16 01:11:10 -0300

    hola

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,16 @@
-function Login(){
-    return(
-        <div>hola login</div>
-    )
+import "./login.css";
+
+function Login() {
+  return (
+    <div className="container">
+      <section>
+        <div className="row">
+          <div className="col-lg-4"></div>
+
+          <div className="col-lg-8"></div>
+        </div>
+      </section>
+    </div>
+  );
 }
-export default Login
+export default Login;
