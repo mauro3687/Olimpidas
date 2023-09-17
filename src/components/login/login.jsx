b1081fde95325568780cc26bffecd1e831a78d66
Ezequiel Gorosito <mauro.gorosito@25watts.ar>
2023-09-17 00:49:12 -0300

    login hecho

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,5 +1,6 @@
 import imagenes from "../../../public/img/imagenes";
 import "./login.css";
+import { useTranslation } from "react-i18next";
 
 function Login() {
   return (
@@ -16,15 +17,21 @@ function Login() {
             <div className="col-lg-12 d-flex justify-content-center">
               <img src={imagenes.logo} alt="" className="logo-derecho" />
             </div>
-            <div className="col-lg-12 d-flex justify-content-center">
-              <h1>Login</h1>
-            </div>
-            <div className="col-lg-12 d-flex justify-content-center">
-              <input type="text" />
-            </div>
-            <div className="col-lg-12 d-flex justify-content-center">
-              <input type="text" />
-            </div>
+
+            <form action="">
+              <div className="col-lg-12 d-flex justify-content-center ">
+                <h1 className="title">Login</h1>
+              </div>
+              <div className="col-lg-12 d-flex justify-content-center mb-4">
+                <Compone placeholder="Username" type="text" />
+              </div>
+              <div className="col-lg-12 d-flex justify-content-center mb-2">
+                <Compone placeholder="Password" type="password" />
+              </div>
+              <div className="col-lg-12 d-flex justify-content-center mb-2">
+                <button className="btn btn-primary boton">Listo</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -32,3 +39,15 @@ function Login() {
   );
 }
 export default Login;
+
+const Compone = (props) => {
+  return (
+    <div>
+      <input
+        type={props.type}
+        className="campos-login"
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+};
