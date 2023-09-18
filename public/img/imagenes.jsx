863b091a4b5e152bea6d4330f1b19db5ac7be9b0
sansan7sdfsdf <santicolee2004@gmail.com>
2023-09-18 19:29:41 -0300

    Agregando Dashboard

--- public/img/imagenes.jsx
+++ public/img/imagenes.jsx
@@ -1,5 +1,21 @@
 import gray from "./imagenes/Gray.png";
+import seg from ".//imagenes/group_615075.png"
+import area from ".//imagenes/hospital_5193680.png"
+import llamada from ".//imagenes/telephone-call_3059502.png"
+import enfer from ".//imagenes/nurse_2800859.png"
+import alerta from ".//imagenes/caution-sign_75243.png"
+import esta from ".//imagenes/graphic_3839535.png"
+import cerra from ".//imagenes/exit-svgrepo-com.png"
+
+
 
 export default {
   logo: gray,
+  grupo:seg,
+  are:area,
+  lla:llamada,
+  enf:enfer,
+  ale:alerta,
+  es:esta,
+  cer:cerra
 };
--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,10 +1,72 @@
+import imagenes from "../../../public/img/imagenes";
 import Navbar from "../navbar/navbar";
 // fijate en main.jsx como hago las rutas
 // aca despues de navbar pone la imagen y listo
 // en menos de 30 minutos lo debes tener terminado
 // si no lo tienen terminado es porque son unos fracasados
+import './dashboard.css'
 
 function Dashboard() {
-  return <Navbar />;
+  return (
+    <div class="wrapper">
+    <div class="sidebar">
+        <h2>Bienvenidos</h2>
+        <br />
+        <br />
+        <ul>
+        
+            <li>
+            <img src={imagenes.grupo} alt="" className="seg" />
+                <a href=""><i class="fas fa-home">
+              </i>Paciente</a>
+              </li>
+            <li>
+              <img src={imagenes.enf} alt="" className="enfer" />
+              <a href="#"><i class="fas fa-user"></i>Enfermero</a>
+              </li>
+            <li>
+              <img src={imagenes.are} alt="" className="area" />
+              <a href="#"><i class="fas fa-address-card"></i>Areas</a>
+              </li>
+            <li>
+            <img src={imagenes.lla} alt="" className="llamada" />
+
+              <a href="#"><i class="fas fa-project-diagram">
+                </i>Llamadas</a></li>
+            <li>
+            <img src={imagenes.ale} alt="" className="alerta" />
+
+              <a href="#"><i class="fas fa-blog">
+                </i>Alerta</a></li>
+            <li>
+            <img src={imagenes.es} alt="" className="esta" />
+
+              <a href="#"><i class="fas fa-address-book">
+                </i>Estadistica</a></li>
+            <li>
+            <img src={imagenes.grupo} alt="" className="seg" />
+
+              <a href="#"><i class="fas fa-address-book">
+                </i>Informacion</a></li>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        
+            <li>
+              <img src={imagenes.cer} alt="" className="cerra" />
+              <a href="#"><i class="fas fa-map-pin">
+                </i>Cerrar sesion</a></li>
+        </ul> 
+    </div>
+    </div>  
+
+    
+  )  
 }
 export default Dashboard;
+
